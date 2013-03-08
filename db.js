c19d8005db0c962f1e12991bc5bd0752bcb88810
Licson Lee <licson0729@gmail.com>
2013-03-08 21:49:51 +0800

    Almost finish

--- db.js
+++ db.js
@@ -6,11 +6,34 @@ module.exports = {
 			host:'127.0.0.1',
 			user:'root',
 			password:'',
-			db:'poll'
+			database:'poll'
 		});
+		/*
+		var services = JSON.parse(process.env.VCAP_SERVICES);
+		var credit = services['mysql-5.1'][0].credentials;
+		var conn = mysql.createConnection({
+			host:credit.host,
+			user:credit.user,
+			password:credit.password,
+			database:credit.name
+		});
+		*/
 		conn.connect(function(err){
-			if(err) console.log('Error connecting to MySQL database',err);
+			if(err){
+				console.log('Error connecting to MySQL database',err);
+				if(err.fatal) process.exit();
+			}
+			//Create the tables if not exists
+			var noop = function(e){
+				!e && console.log('Table creation succeed.');
+			};
+			conn.query('SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";',noop);
+			conn.query('SET time_zone = "+00:00";',noop);
+			conn.query('CREATE TABLE IF NOT EXISTS `answers` (`question_id` varchar(40) NOT NULL,`poll_id` varchar(25) NOT NULL,`id` varchar(40) NOT NULL,`value` text NOT NULL,`date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,PRIMARY KEY (`id`),KEY `poll_id` (`poll_id`)) ENGINE=InnoDB DEFAULT CHARSET=utf8;',noop);
+			conn.query('CREATE TABLE IF NOT EXISTS `polls` ( `id` varchar(25) NOT NULL, `name` varchar(100) NOT NULL, `desc` text, `keywords` varchar(200) DEFAULT NULL, `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY (`id`) ) ENGINE=InnoDB DEFAULT CHARSET=utf8;',noop);
+			conn.query('CREATE TABLE IF NOT EXISTS `questions` ( `id` varchar(40) NOT NULL, `belongs` varchar(25) NOT NULL, `type` varchar(20) NOT NULL, `name` text NOT NULL, `choices` text, `required` tinyint(1) NOT NULL DEFAULT \'1\', PRIMARY KEY (`id`) ) ENGINE=InnoDB DEFAULT CHARSET=utf8;',noop);
 		});
+		
 		return conn;
 	}
 };
--- index.js
+++ index.js
@@ -14,23 +14,36 @@ var genID = function(len){
 	
 	return ret;
 };
-
-app.use(express.logger());
-app.use(express.static(__dirname));
+//Custom logger
+app.use(function(req,res,next){
+	console.log('Request page %s from %s',req.url,req.connection.remoteAddress);
+	next();
+});
+app.use('/assets',express.static(__dirname+'/assets/'));
 app.use(express.bodyParser());
 
 app.get('/',function(req,res){
-	conn.query('SELECT * FROM `polls` ORDER BY `created_at` DESC OFFSET 0,5',function(err,data){
-		res.end(view.page('index',{polls:data}));
+	conn.query('SELECT * FROM `polls` ORDER BY `created_at` DESC',function(err,data){
+		res.send(view.page('index',{polls:data}));
 	});
 });
 
+app.get('/thanks',function(req,res){
+	res.send(view.page('thanks',{}));
+});
+
+app.get('/about',function(req,res){
+	res.send(view.page('about',{}));
+});
+
 app.get('/question/:id',function(req,res){
-	conn.query('SELECT * FROM `polls` WHERE `id` = ?',req.params.id,function(err,meta){
-		conn.query('SELECT * FROM `questions` WHERE `belongs` = ?',req.params.id,function(err,data){
+	var meta;
+	conn.query('SELECT * FROM `polls` WHERE `id` = ?',[req.params.id],function(err,info){
+		meta = info;
+		conn.query('SELECT * FROM `questions` WHERE `belongs` = ? ORDER BY `name` ASC',[req.params.id],function(err,data){
 			res.end(view.page('question',{
 				id:req.params.id,
-				info:meta,
+				meta:meta,
 				questions:data
 			}));
 		});
@@ -38,6 +51,7 @@ app.get('/question/:id',function(req,res){
 });
 
 app.post('/question/:id/submit',function(req,res){
+	var success = true;
 	for(var q in req.body){
 		var data = {
 			question_id:req.params.id,
@@ -47,15 +61,12 @@ app.post('/question/:id/submit',function(req,res){
 		};
 		conn.query('INSERT INTO `answers` SET ?',data,function(err){
 			if(err){
-				res.end(JSON.stringify({success:false}));
+				success = false;
 			}
 		});
 	}
 	
-	try{
-		res.end(JSON.stringify({success:true}));
-	}
-	catch(e){}
+	res.json({success:success});
 });
 
 app.get('/create',function(req,res){
@@ -63,23 +74,101 @@ app.get('/create',function(req,res){
 });
 
 app.post('/create/save/basic_info',function(req,res){
-	res.writeHead(200,{'Content-type':'application/json'});
 	var data = req.body;
 	data.id = genID(25);
-	conn.query('INSERT INTO `polls` SET ?',data,function(err){
-		if(!err){
-			conn.query('SELECT id FROM `polls` WHERE `name` = ? AND `desc` = ? AND `keywords` = ?',[data.name,data.desc,data.keywords],function(err,data){
-				if(!err){
-					res.end(JSON.stringify({success:true,id:data.id}));
+	if(!(data.name == '' || data.desc == '' || data.keywords == '')){
+		conn.query('INSERT INTO `polls` SET ?',data,function(err){
+			if(!err){
+				res.json({success:true,id:data.id});
+			}
+			else {
+				res.json({success:false,message:'Database Error, please try later.'});
+				console.log(err);
+			}
+		});
+	}
+	else {
+		res.json({success:false,message:'All fields were required.'});
+	}
+});
+
+app.post('/create/save/questions',function(req,res){
+	var success = true;
+	for(var i = 0; i < req.body.questions.belongs.length; i++){
+		var q = {
+			belongs:req.body.questions.belongs[i],
+			name:req.body.questions.name[i],
+			type:req.body.questions.type[i],
+			choices:req.body.questions.choices[i],
+			required:req.body.questions.required[i],
+			id:genID(40)
+		};
+		conn.query('INSERT INTO `questions` SET ?',q,function(err){
+			if(err){
+				success = false
+			}
+		});
+	}
+	var message = {success:success};
+	if(!success){
+		message.message = "Database error, please try again later.";
+	}
+	res.json(message);
+});
+
+app.get('/stats/:id',function(req,res){
+	var id = req.params.id;
+	conn.query('SELECT * FROM `questions` WHERE `belongs` = ? ORDER BY `name` ASC',[req.params.id],function(err,questions){
+		conn.query('SELECT count(*) AS `count` FROM `answers` WHERE `question_id` = ?',[id],function(err,data){
+			var t_count = data[0].count;
+			conn.query('SELECT count(*) AS `count` FROM (SELECT DISTINCT `poll_id`, `question_id` FROM `answers`) AS q WHERE q.`question_id` = ? GROUP BY q.`question_id`',[id],function(err,data){
+				var q_count = data[0].count;
+				conn.query('SELECT * FROM `polls` WHERE `id` = ?',[id],function(err,meta){
+					res.send(view.page('stats',{count:{users:t_count/q_count,total:t_count},meta:meta[0],id:id,questions:questions}));
+				});
+			});
+		});
+	});
+});
+
+app.get('/stats/:id/time',function(req,res){
+	conn.query('SELECT count(*) AS `count` FROM (SELECT DISTINCT `poll_id`, `question_id` FROM `answers`) AS q WHERE q.`question_id` = ? GROUP BY q.`question_id`',[req.params.id],function(err,data){
+		var q_count = data[0].count;
+		conn.query('SELECT COUNT(*) AS `count`, `date` FROM `answers` WHERE `question_id` = ? GROUP BY `date` ORDER BY `date` ASC',[req.params.id],function(err,data){
+			var intital_date = null;
+			var _return = [];
+			var _return_i = 0;
+			for(var i = 0; i < data.length; i++){
+				data[i].date.setHours(0);
+				data[i].date.setSeconds(0);
+				if(intital_date == null || (data[i].date.getFullYear() !== intital_date.getFullYear() || data[i].date.getMonth() !== intital_date.getMonth() || data[i].date.getDate() !== intital_date.getDate())){
+					_return.push([data[i].date.getTime(),data[i].count/q_count]);
+					_return_i = _return.length == 1 ? 0 : _return_i+1;
 				}
 				else {
-					res.end(JSON.stringify({success:false}));
+					_return[_return_i][1] += (data[i].count/q_count);
 				}
-			});
-		}
-		else {
-			res.end(JSON.stringify({success:false}));
-		}
+				intital_date = data[i].date;
+			}
+			res.json(_return);
+		});
+	});
+});
+
+app.get('/stats/question/:id',function(req,res){
+	conn.query('SELECT * FROM `questions` WHERE `id` = ?',[req.params.id],function(err,type){
+		conn.query('SELECT COUNT(*) AS `count`, `value` FROM `answers` WHERE `poll_id` = ? GROUP BY `value` ORDER BY `value` ASC',[req.params.id],function(err,data){
+			if(type[0].type == 'mc'){
+				var _data = [];
+				for(var i = 0; i < data.length; i++){
+					_data.push({data:[[0,data[i].count]],label:data[i].value});
+				}
+				res.json(_data);
+			}
+			else {
+				res.json({html:view.page('ques_dialog',{})});
+			}
+		});
 	});
 });
 
